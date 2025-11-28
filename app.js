36c1fc2617231670c99e84c33c398ca84b0a29a8
Blopinpg1 <bibekoli615@gmail.com>
2025-11-28 23:21:26 +0545

    Menu.js created

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-window.addEventListener("DOMContentLoaded", () => {
-  let nav = document.querySelector("nav");
-  console.log(nav);
-  nav.innerHTML = `
-  <h2>Hello DOM <h2>
-  <p>This content is loaded dynamically.</p>
-`;
+import Store from "/services/Store.js";
+import API from "/services/API.js";
+import { loadData } from "./services/Menu";
+
+window.app = {};
+app.store = Store;
+
+window.addEventListener("DOMContentLoaded", async () => {
+  loadData();
 });
 
-window.addEventListener("DOMContentLoaded", () => {});
+// const $ = function (args) {
+//   return document.querySelector(args);
+// };
+// const $$ = function (args) {
+//   return document.querySelectorAll(args);
+// }; // shorthand for querySelector and querySelectorAll
 
-const $ = function (args) {
-  return document.querySelector(args);
-};
-const $$ = function (args) {
-  return document.querySelectorAll(args);
-};
-
-HTMLElement.prototype.on = function (a, b, c) {
-  return this.addEventListener(a, b, c);
-};
-HTMLElement.prototype.off = function (a, b) {
-  return this.removeEventListener(a, b);
-};
-HTMLElement.prototype.$ = function (s) {
-  return this.querySelector(s);
-};
-HTMLElement.prototype.$$ = function (s) {
-  return this.querySelectorAll(s);
-};
+// HTMLElement.prototype.on = function (a, b, c) {
+//   return this.addEventListener(a, b, c);
+// };
+// HTMLElement.prototype.off = function (a, b) {
+//   return this.removeEventListener(a, b);
+// };
+// HTMLElement.prototype.$ = function (s) {
+//   return this.querySelector(s);
+// };
+// HTMLElement.prototype.$$ = function (s) {
+//   return this.querySelectorAll(s);
+// };
+// // shorthand for adding methods to HTMLElement prototype
